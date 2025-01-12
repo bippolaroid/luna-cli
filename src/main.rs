e44cfa5d0f2860d2599023e1a1061a76cd4e4979
bippolaroid <bippolaroid@proton.me>
2025-01-12 00:16:00 -0500

    started user message chaining

--- src/main.rs
+++ src/main.rs
@@ -1,18 +1,49 @@
-use rocket::{get, launch, routes};
+use once_cell::sync::Lazy;
+use serde_json::{json, Value};
 use std::io::{self, Write};
 use std::process::Command;
+use tokio::sync::Mutex;
 
-#[launch]
-async fn rocket() -> _ {
+pub static BODY: Lazy<Mutex<Value>> = Lazy::new(|| {
+    Mutex::new(json!({
+        "model": "bippy/cli-tool",
+        "messages": [{
+            "role": "system",
+            "content": "You are an assistant that directly executes in a Windows 11 command prompt.
+
+        ASSISTANT KEYWORDS:
+        The role 'user' is the user's request.
+        The role 'assistant' is your previous response.
+        The role 'output' is the output response of the command prompt.
+
+        ASSISTANT POLICY:
+        You must only respond with unformatted Windows 11 Command Prompt commands or scripts.
+        You must pay specific attention to capitalization and syntax.
+        You must not simulate or emulate execution or command prompt output.
+        You must respond very explicitly with what the user requests - nothing more or less.
+        You must only respond one line at a time.
+        You must adhere to these rules at all times - no exceptions.",
+        },
+        {
+            "role": "output",
+            "content": "C:\\Users\\mdeez>"
+
+        }],
+        "stream": false
+    }))
+});
+
+#[tokio::main]
+async fn main() {
     display_menu().await;
-    rocket::build().mount("/", routes![hello])
 }
 
 async fn display_menu() {
+    println!("\nLunaCLI is now running...");
+    println!("\nType 'exit' to exit.");
+
     loop {
-        println!("\nLunaCLI Menu\n------------------------");
-        println!("1. Generate\n2. Exit\n------------------------");
-        print!("Please enter your choice: ");
+        print!("\n[LunaCLI] C:\\> ");
         io::stdout().flush().unwrap();
 
         let mut choice: String = String::new();
@@ -20,56 +51,34 @@ async fn display_menu() {
             .read_line(&mut choice)
             .expect("Failed to read line.");
         match choice.trim() {
-            "1" => {
-                println!("\nGenerating...");
-                send_request().await;
-            }
-            "2" => {
-                println!("\nExiting...");
+            "exit" => {
+                println!("\nExiting...\n");
                 return;
             }
-            _ => println!("\nInvalid choice. Please try again."),
+            _ => {
+                println!("\nGenerating...");
+                send_request(choice).await;
+            }
         }
     }
 }
 
-async fn send_request() {
+async fn send_request(user_request: String) {
     let client: reqwest::Client = reqwest::Client::new();
 
-    let body: serde_json::Value = serde_json::json!({
-        "model": "bippy/cli-tool",
-        "messages": [{
-            "role": "system",
-            "content": "You are an assistant that directly executes in a Windows 11 command prompt.
+    let mut body: tokio::sync::MutexGuard<'_, Value> = BODY.lock().await;
 
-            ASSISTANT KEYWORDS:
-            The role 'user' is the user's request.
-            The role 'assistant' is your previous response.
-            The role 'output' is the output response of the command prompt.
-
-            ASSISTANT POLICY:
-            You must only respond with unformatted Windows 11 Command Prompt commands or scripts.
-            You must pay specific attention to capitalization and syntax.
-            You must not simulate or emulate execution or command prompt output.
-            You must only respond one line at a time.
-            You must adhere to these rules at all times - no exceptions.",
-        },
-        {
-            "role": "output",
-            "content": "C:\\Users\\mdeez>"
-
-        },
-        {
+    if let Some(Value::Array(messages)) = body.get_mut("messages") {
+        messages.push(json!({
             "role": "user",
-            "content": "List the directories."
-        }],
-        "stream": false
-    });
+            "content": user_request,
+        }));
+    }
     println!("\nUSER INPUT: {body}");
 
     let response: Result<reqwest::Response, reqwest::Error> = client
         .post("http://localhost:11434/api/chat")
-        .json(&body)
+        .json(&*body)
         .send()
         .await;
     match response {
@@ -81,7 +90,7 @@ async fn send_request() {
                     Ok(json_response) => {
                         if let Some(response_value) = json_response.get("message") {
                             if let Some(content_value) = response_value.get("content") {
-                                println!("ASSISTANT OUTPUT: {}", content_value);
+                                println!("\nASSISTANT OUTPUT: {}", content_value);
                             }
                         } else {
                             println!("Response not found.");
@@ -108,9 +117,4 @@ fn execute_command(command: &str) {
         String::from_utf8_lossy(&output.stdout)
     );
     println!("Command error: {}", String::from_utf8_lossy(&output.stderr));
-}
-
-#[get("/")]
-fn hello() -> &'static str {
-    "Hello, world!"
 }
