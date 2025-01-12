9b30a8a8ee391ba639f3de3b91e252154ad6f64d
bippolaroid <bippolaroid@proton.me>
2025-01-11 23:26:50 -0500

    using json and prompt chaining

--- src/main.rs
+++ src/main.rs
@@ -1,133 +1,113 @@
-use std::collections::HashMap;
+use rocket::{get, launch, routes};
 use std::io::{self, Write};
 use std::process::Command;
-use tokio::sync::Mutex;
-
-use serde_json::Value;
-
-#[macro_use]
-extern crate rocket;
-
-lazy_static::lazy_static! {
-    static ref PREVIOUS_COMMAND: Mutex<String> = Mutex::new(String::from("None"));
-    static ref ROOT: Mutex<String> = Mutex::new(String::from("C:\\Users\\guest\\Desktop"));
-    static ref PATH: Mutex<String> = Mutex::new(String::from("C:\\Users\\guest\\Desktop"));
-    static ref PREV_OUTPUT: Mutex<String> = Mutex::new(String::from(""));
-}
 
 #[launch]
 async fn rocket() -> _ {
-    show_menu().await;
+    display_menu().await;
     rocket::build().mount("/", routes![hello])
 }
 
-async fn show_menu() {
+async fn display_menu() {
     loop {
-        println!("\nLunaCLI Menu");
-        println!("------------------------");
-        println!("1. Generate");
-        println!("2. Exit");
-        println!("------------------------");
+        println!("\nLunaCLI Menu\n------------------------");
+        println!("1. Generate\n2. Exit\n------------------------");
         print!("Please enter your choice: ");
-
         io::stdout().flush().unwrap();
 
         let mut choice: String = String::new();
         io::stdin()
             .read_line(&mut choice)
             .expect("Failed to read line.");
-
         match choice.trim() {
             "1" => {
-                println!("Generating...");
-                generate().await;
+                println!("\nGenerating...");
+                send_request().await;
             }
             "2" => {
-                println!("Exiting...");
+                println!("\nExiting...");
                 return;
             }
-            _ => println!("Invalid choice. Please try again."),
+            _ => println!("\nInvalid choice. Please try again."),
         }
     }
 }
 
-async fn generate() {
-    let mut map: HashMap<&str, serde_json::Value> = HashMap::new();
-
-    map.insert(
-        "model",
-        serde_json::Value::String("bippy/cli-tool".to_string()),
-    );
-
-    let prev_output: tokio::sync::MutexGuard<'_, String> = PREV_OUTPUT.lock().await;
-    let path: tokio::sync::MutexGuard<'_, String> = PATH.lock().await;
-
-    map.insert(
-        "prompt",
-        serde_json::Value::String(format!("{}\n{}", &*prev_output, &path)),
-    );
-    map.insert("stream", serde_json::Value::Bool(false));
-
-    let json_body: String = serde_json::to_string(&map).expect("Failed to serialize");
-    println!("{}", json_body);
-
+async fn send_request() {
     let client: reqwest::Client = reqwest::Client::new();
-    let res: Result<reqwest::Response, reqwest::Error> = client
-        .post("http://localhost:11434/api/generate")
-        .json(&map)
+
+    let body: serde_json::Value = serde_json::json!({
+        "model": "bippy/cli-tool",
+        "messages": [{
+            "role": "system",
+            "content": "You are an assistant that directly executes in a Windows 11 command prompt.
+
+            ASSISTANT KEYWORDS:
+            The role 'user' is the user's request.
+            The role 'assistant' is your previous response.
+            The role 'output' is the output response of the command prompt.
+
+            ASSISTANT POLICY:
+            You must only respond with unformatted Windows 11 Command Prompt commands or scripts.
+            You must pay specific attention to capitalization and syntax.
+            You must not simulate or emulate execution or command prompt output.
+            You must only respond one line at a time.
+            You must adhere to these rules at all times - no exceptions.",
+        },
+        {
+            "role": "output",
+            "content": "C:\\Users\\mdeez>"
+
+        },
+        {
+            "role": "user",
+            "content": "List the directories."
+        }],
+        "stream": false
+    });
+    println!("\nUSER INPUT: {body}");
+
+    let response: Result<reqwest::Response, reqwest::Error> = client
+        .post("http://localhost:11434/api/chat")
+        .json(&body)
         .send()
         .await;
-
-    match res {
-        Ok(response) => {
-            let response_text: String = response.text().await.unwrap();
-            if let Ok(parsed) = serde_json::from_str::<Value>(&response_text) {
-                if let Some(response_field) = parsed.get("response") {
-                    if let Value::String(ref response_str) = response_field {
-                        let output: std::process::Output = Command::new("cmd")
-                            .args(&["/C"])
-                            .args(&[response_str])
-                            .output()
-                            .expect("Failed to execute process.");
-
-                        if output.status.success() {
-                            let stdout: std::borrow::Cow<'_, str> =
-                                String::from_utf8_lossy(&output.stdout);
-                                let mut prev_output: tokio::sync::MutexGuard<'_, String> = PREV_OUTPUT.lock().await;
-                                *prev_output = stdout.to_string();
-                            println!("\nEXECUTING CODE: {}", response_str);
-                            println!("- - - - - - - - - -\n");
-                            println!("{}", stdout);
-                            println!("- - - - - - - - - -");
-                            push_command(response_str).await;
-                            let previous_command: tokio::sync::MutexGuard<'_, String> =
-                                PREVIOUS_COMMAND.lock().await;
-                            println!("Previous: {:?}", *previous_command);
+    match response {
+        Ok(res) => match res.text().await {
+            Ok(text) => {
+                let json: Result<serde_json::Value, serde_json::Error> =
+                    serde_json::from_str(&text);
+                match json {
+                    Ok(json_response) => {
+                        if let Some(response_value) = json_response.get("message") {
+                            if let Some(content_value) = response_value.get("content") {
+                                println!("ASSISTANT OUTPUT: {}", content_value);
+                            }
                         } else {
-                            let stderr: std::borrow::Cow<'_, str> =
-                                String::from_utf8_lossy(&output.stderr);
-                            println!("Error: {}", stderr);
-                            println!("{}", response_str);
+                            println!("Response not found.");
+                            println!("{}", json_response);
                         }
-                    } else {
-                        println!("Response field is not a string.");
                     }
-                } else {
-                    println!("Failed to parse response field.");
+                    Err(e) => eprintln!("\nError parsing: {}", e),
                 }
-            } else {
-                println!("Error parsing response text.");
             }
-        }
-        Err(_) => {
-            println!("Request error");
-        }
+            Err(e) => eprintln!("\nError reading response: {}", e),
+        },
+        Err(e) => eprintln!("\nError sending request: {}", e),
     }
 }
 
-async fn push_command(string: &str) {
-    let mut previous_command: tokio::sync::MutexGuard<'_, String> = PREVIOUS_COMMAND.lock().await;
-    *previous_command = String::from(string);
+fn execute_command(command: &str) {
+    println!("Command: {}", command);
+    let output = Command::new("cmd")
+        .args(&["/C", command])
+        .output()
+        .expect("Failed to execute command");
+    println!(
+        "Command output: {}",
+        String::from_utf8_lossy(&output.stdout)
+    );
+    println!("Command error: {}", String::from_utf8_lossy(&output.stderr));
 }
 
 #[get("/")]
